9e6a869a16b6a2dfbfdaf10c893d2a8c575375b7
NevenaAl <51334038+NevenaAl@users.noreply.github.com>
2019-12-19 01:57:30 +0100

    Implementirano gubljenje zivota.

--- DonkeyKong/Main.py
+++ DonkeyKong/Main.py
@@ -4,10 +4,14 @@ import random
 from PyQt5.QtWidgets import QWidget, QMainWindow, QPushButton, QHBoxLayout, QApplication, QLabel, QVBoxLayout, QGridLayout, QSizePolicy
 from PyQt5.QtGui import QImage, QPalette, QBrush, QPixmap
 from PyQt5.QtCore import QSize, Qt
+from PyQt5 import QtWidgets, QtGui
 import sys
 import time
+from tkinter import *
 import threading
 from threading import Thread
+
+
 
 
 class DonkeyKong(QLabel):
@@ -26,7 +30,7 @@ class DonkeyKong(QLabel):
     def getPosition(self):
         for x in range(len(self.map)):
             for y in range(len(self.map[x])):
-                if self.map[x][y] == 6 or self.map[x][y] == 8:
+                if self.map[x][y] == 6 or self.map[x][y] == 8 or self.map[x][y] >= 9:
                         self.DonkeyX = x
                         self.DonkeyY = y
                         return
@@ -69,6 +73,29 @@ class DonkeyKong(QLabel):
                         #self.printMap()
                         time.sleep(0.5)
 
+class Lives(QLabel):
+    def __init__(self, lives,  parent=None):
+        super().__init__(parent)
+        self.lives = lives
+        self.change_hearts(self.lives)
+
+    def change_hearts(self, lives):
+        self.lives = lives
+        if self.lives == 3:
+            self.setGeometry(9, 10, 100, 70)
+            pix = QPixmap('3hearts.jpg')
+            pixx = pix.scaled(QSize(100, 70))
+            self.setPixmap(pixx)
+        elif self.lives == 2:
+            self.setGeometry(9, 10, 100, 70)
+            pix = QPixmap('2hearts.jpg')
+            pixx = pix.scaled(QSize(100, 70))
+            self.setPixmap(pixx)
+        elif self.lives == 1:
+            self.setGeometry(9, 10, 100, 70)
+            pix = QPixmap('1heart.jpg')
+            pixx = pix.scaled(QSize(100, 70))
+            self.setPixmap(pixx)
 
 class Princess(QLabel):
     def __init__(self, parent=None):
@@ -93,7 +120,7 @@ class Princess(QLabel):
 
 
 class Mover(QLabel):
-    def __init__(self, map, parent=None):
+    def __init__(self, map, livesWidget, parent=None):
         super().__init__(parent)
         self.setGeometry(-8, 621, 50, 70)
         pix = QPixmap('ItsAMeRight.png')
@@ -102,12 +129,121 @@ class Mover(QLabel):
         self.map = map
         self.PlayerX = 0
         self.PlayerY = 0
+        self.lives = 3
+        self.th = Thread(target=self.check_lives, args=(livesWidget,))
+        self.th.start()
+
+    def check_lives(self, livesWidget):
+        while True:
+            self.getPosition()
+            if self.map[self.PlayerX][self.PlayerY] >= 9 or self.map[self.PlayerX - 1][self.PlayerY] >= 9:
+                  self.printMap()
+                  #self.map[self.PlayerX][self.PlayerY] = self.map[self.PlayerX][self.PlayerY] - 3
+                  #self.map[self.PlayerX - 1][self.PlayerY] = self.map[self.PlayerX - 1][self.PlayerY] - 3
+                  self.setGeometry(-8, 621, 50, 70)
+                  pix = QPixmap('ItsAMeRight.png')
+                  pixx = pix.scaled(QSize(50, 70))
+                  self.setPixmap(pixx)
+                  self.PlayerX = 0
+                  self.PlayerY = 0
+                  self.map = \
+                      [[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+                        1],
+                       [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                        1],
+                       [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4, 4, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                        1],
+                       [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4, 4, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                        1],
+                       [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4, 4, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                        1],
+                       [1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 1,
+                        1],
+                       [1, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 6, 6, 6, 6, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0,
+                        1],
+                       [1, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 6, 6, 6, 6, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0,
+                        1],
+                       [1, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 6, 6, 6, 6, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0,
+                        1],
+                       [1, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 6, 6, 6, 6, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0,
+                        1],
+                       [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+                        1],
+                       [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                        1],
+                       [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                        1],
+                       [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                        1],
+                       [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                        1],
+                       [1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1,
+                        1],
+                       [1, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0,
+                        1],
+                       [1, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0,
+                        1],
+                       [1, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0,
+                        1],
+                       [1, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0,
+                        1],
+                       [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+                        1],
+                       [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                        1],
+                       [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                        1],
+                       [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                        1],
+                       [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                        1],
+                       [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+                        1],
+                       [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                        1],
+                       [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                        1],
+                       [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                        1],
+                       [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                        1],
+                       [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+                        1],
+                       [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                        1],
+                       [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                        1],
+                       [1, 3, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                        1],
+                       [1, 3, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                        1],
+                       [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+                        1],
+                       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                        0],
+                       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                        0],
+                       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                        0],
+                       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                        0],
+                       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                        0]]
+                  if self.lives - 1 > 0:
+                      self.lives = self.lives-1
+                      if self.lives == 2:
+                          livesWidget.change_hearts(self.lives)
+                      elif self.lives == 1:
+                          livesWidget.change_hearts(self.lives)
+                  self.getPosition()
+            time.sleep(0.5)
+
 
     def getPosition(self):
         self.playerDrawn = 0
         for x in range(len(self.map)):
             for y in range(len(self.map[x])):
-                if self.map[x][y] == 3 or self.map[x][y] == 5:
+                if self.map[x][y] == 3 or self.map[x][y] == 5 or self.map[x][y] >= 9:
                     if self.playerDrawn == 0:
                         self.playerDrawn = 1
                     else:
@@ -129,14 +265,19 @@ class Mover(QLabel):
                 self.move(self.x(), self.y() - 19)
                 self.map[self.PlayerX][self.PlayerY] = self.map[self.PlayerX][self.PlayerY] - 3
                 self.map[self.PlayerX - 2][self.PlayerY] = self.map[self.PlayerX - 2][self.PlayerY] + 3
+
                 #self.printMap()
         elif event.key() == Qt.Key_Down:
+            self.printMap()
+            print(self.PlayerY)
+            print(self.PlayerX)
             if self.y() + 19 <= 630:
                 if self.map[self.PlayerX+1][self.PlayerY] == 5 or self.map[self.PlayerX + 1][self.PlayerY] == 2:
                     self.move(self.x(), self.y() + 19)
                     self.map[self.PlayerX + 1][self.PlayerY] = self.map[self.PlayerX + 1][self.PlayerY] + 3
                     self.map[self.PlayerX - 1][self.PlayerY] = self.map[self.PlayerX - 1][self.PlayerY] - 3
-                    #self.printMap()
+
+
         elif event.key() == Qt.Key_Left:
             if self.x() - 18 >= -8:
                 if not (self.map[self.PlayerX+1][self.PlayerY] == 2 and self.map[self.PlayerX + 1][self.PlayerY+1] == 0):
@@ -148,7 +289,8 @@ class Mover(QLabel):
                     self.map[self.PlayerX-1][self.PlayerY] = self.map[self.PlayerX-1][self.PlayerY] - 3
                     self.map[self.PlayerX][self.PlayerY-1] = self.map[self.PlayerX][self.PlayerY-1] + 3
                     self.map[self.PlayerX-1][self.PlayerY-1] = self.map[self.PlayerX-1][self.PlayerY-1] + 3
-                    #self.printMap()
+
+                   # self.printMap()
         elif event.key() == Qt.Key_Right:
             if self.x() + 18 <= 532:
                 if not (self.map[self.PlayerX + 1][self.PlayerY] == 2 and self.map[self.PlayerX + 1][self.PlayerY + 1] == 0):
@@ -160,6 +302,7 @@ class Mover(QLabel):
                     self.map[self.PlayerX-1][self.PlayerY] = self.map[self.PlayerX-1][self.PlayerY] - 3
                     self.map[self.PlayerX][self.PlayerY+1] = self.map[self.PlayerX][self.PlayerY+1] + 3
                     self.map[self.PlayerX-1][self.PlayerY+1] = self.map[self.PlayerX-1][self.PlayerY+1] + 3
+
                     #self.printMap()
         else:
             QLabel.keyPressEvent(self, event)
@@ -168,6 +311,8 @@ class Mover(QLabel):
 class MainWindow(QWidget):
     def __init__(self):
         super().__init__()
+        self.setWindowIcon(QtGui.QIcon('doKo.png'))
+        self.setWindowTitle("Donkey Kong")
         self.initUI()
 
     def initUI(self):
@@ -242,27 +387,29 @@ class MainWindow(QWidget):
         self.show()
 
     def on_start(self):
-        oImage = QImage("Background.png")
+        oImage = QImage("Background2.png")
         sImage = oImage.scaled(QSize(600, 700))
         palette = QPalette()
         palette.setBrush(QPalette.Window, QBrush(sImage))
         self.setPalette(palette)
         self.startButton.hide()
-
+        self.lives = 3
         self.PrincessWidget = QWidget()
         self.MarioWidget = QWidget()
         self.DonkeyWidget = QWidget()
+        self.LivesWidget = QWidget()
 
-        # self.PrincessWidget.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+
         self.hbox.addWidget(self.PrincessWidget, 1, 1)
-        # self.MarioWidget.setSizePolicy(QSizePolicy.Expanding,QSizePolicy.Expanding)
         self.hbox.addWidget(self.MarioWidget, 1, 1)
-        # self.DonkeyWidget.setSizePolicy(QSizePolicy.Expanding,QSizePolicy.Expanding)
         self.hbox.addWidget(self.DonkeyWidget, 1, 1)
+        self.hbox.addWidget(self.LivesWidget, 1, 1)
 
-        self.mover = Mover(self.map, self.MarioWidget)
+        self.livesWidget = Lives(self.lives, self.LivesWidget)
+        self.mover = Mover(self.map, self.livesWidget, self.MarioWidget)
         self.princess = Princess(self.PrincessWidget)
         self.donkey = DonkeyKong(self.map, self.DonkeyWidget)
+
         self.mover.setFocus()
         #self.th = Thread(target=self.printMap(), args=())
         #self.th.start()
